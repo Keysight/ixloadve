1e43596beaa3a15fda56d11d01270685c46eb3c2
armdupre <a-joseph.dupre@keysight.com>
2023-08-22 10:53:44 -0400

    Initial Checkin.

---
+++ aws/Terraform/IxLoad/IxLoad-on-AWS-1-App-2-Agents-1-VPC-1-Public-Subnet-2-Private-Subnets/main.tf
@@ -0,0 +1,59 @@
+module "App" {
+	source = "armdupre/module-ixload-app/aws"
+	version = "9.36.0"
+	Eth0SecurityGroupId = module.Vpc.PublicSecurityGroup.id
+	Eth0SubnetId = module.Vpc.PublicSubnet.id
+	InstanceType = local.AppInstanceType
+	SshKeyName = local.AppSshKeyName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	depends_on = [
+		module.Vpc
+	]
+}
+
+module "Agent1" {
+	source = "armdupre/module-ixload-agent/aws"
+	version = "9.36.0"
+	Eth0SecurityGroupId = module.Vpc.PublicSecurityGroup.id
+	Eth0SubnetId = module.Vpc.PublicSubnet.id
+	Eth1SecurityGroupId = module.Vpc.PrivateSecurityGroup.id
+	Eth1SubnetId = module.Vpc.Private1Subnet.id
+	InstanceId = local.Agent1InstanceId
+	InstanceType = local.AgentInstanceType
+	PlacementGroupId = aws_placement_group.PlacementGroup.id
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	depends_on = [
+		aws_placement_group.PlacementGroup,
+		module.Vpc
+	]
+}
+
+module "Agent2" {
+	source = "armdupre/module-ixload-agent/aws"
+	version = "9.36.0"
+	Eth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
+	Eth0SecurityGroupId = module.Vpc.PublicSecurityGroup.id
+	Eth0SubnetId = module.Vpc.PublicSubnet.id
+	Eth1PrivateIpAddresses = local.Agent2Eth1PrivateIpAddresses
+	Eth1SecurityGroupId = module.Vpc.PrivateSecurityGroup.id
+	Eth1SubnetId = module.Vpc.Private2Subnet.id
+	InstanceId = local.Agent2InstanceId
+	InstanceType = local.AgentInstanceType
+	PlacementGroupId = aws_placement_group.PlacementGroup.id
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	depends_on = [
+		aws_placement_group.PlacementGroup,
+		module.Vpc
+	]
+}
+
+resource "aws_placement_group" "PlacementGroup" {
+	name = local.PlacementGroupName
+	strategy = local.PlacementGroupStrategy
+}
