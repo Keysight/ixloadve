88a15e83613d74c1c2a8a5dd2662f8238b08df4b
armdupre <a-joseph.dupre@keysight.com>
2023-08-24 10:38:43 -0400

    Initial Checkin.

---
+++ microsoft-azure/Deployment/Terraform/IxLoad/IxLoad-on-Azure-1-App-2-Agents-1-Vnet-1-Public-Subnet-2-Private-Subnets/vnet.tf
@@ -0,0 +1,25 @@
+module "Vnet" {
+	source = "armdupre/module-1-vnet-1-public-subnet-2-private-subnets/azurerm"
+	PublicSecurityRuleSourceIpPrefix = local.PublicSecurityRuleSourceIpPrefix
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	Tag = local.AppTag
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	Version = local.AppVersion
+}
+
+resource "azurerm_network_security_rule" "PublicRdpSecurityRule" {
+	name = local.PublicRdpSecurityRuleName
+	description = "Allow RDP"
+	protocol = "Tcp"
+	source_port_range = "*"
+	destination_port_range = "3389"
+	source_address_prefix = local.PublicSecurityRuleSourceIpPrefix
+	destination_address_prefix = "*"
+	access = "Allow"
+	priority = 103
+	direction = "Inbound"
+	resource_group_name = azurerm_resource_group.ResourceGroup.name
+	network_security_group_name = module.Vnet.PublicNetworkSecurityGroup.name
+}
