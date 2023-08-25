93a842016bb03f7e6e550711dc1659f897eb8859
armdupre <a-joseph.dupre@keysight.com>
2023-08-25 15:20:06 -0400

    Initial Checkin.

---
+++ google-cloud-platform/Deployment/Terraform/IxLoad/IxLoad-on-GCP-1-App-2-Agents-2-VPCs-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -0,0 +1,59 @@
+module "App" {
+	source = "armdupre/module-ixload-app/google"
+	version = "9.36.0"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	MachineType = local.AppMachineType
+	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = local.ZoneName
+	depends_on = [
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
+	]
+}
+
+module "Agent1" {
+	source = "armdupre/module-ixload-agent/google"
+	version = "9.36.0"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	Eth1SubnetName = module.Vpc.PrivateSubnet.name
+	Eth1VpcNetworkName = module.Vpc.PrivateVpcNetwork.name
+	InstanceId = local.Agent1InstanceId
+	MachineType = local.AgentMachineType
+	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = local.ZoneName
+	depends_on = [
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
+	]
+}
+
+module "Agent2" {
+	source = "armdupre/module-ixload-agent/google"
+	version = "9.36.0"
+	Eth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	Eth1PrivateIpAddress = local.Agent2Eth1PrivateIpAddress
+	Eth1PrivateIpAliases = local.Agent2Eth1PrivateIpAliases
+	Eth1SubnetName = module.Vpc.PrivateSubnet.name
+	Eth1VpcNetworkName = module.Vpc.PrivateVpcNetwork.name
+	InstanceId = local.Agent2InstanceId
+	MachineType = local.AgentMachineType
+	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = local.ZoneName
+	depends_on = [
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
+	]
+}
