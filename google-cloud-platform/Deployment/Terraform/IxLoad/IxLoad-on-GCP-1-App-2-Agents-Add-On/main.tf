aafd9833be1e1d78cd42d25da1ef77de4dd5310c
armdupre <a-joseph.dupre@keysight.com>
2023-08-25 15:25:09 -0400

    Initial Checkin.

---
+++ google-cloud-platform/Deployment/Terraform/IxLoad/IxLoad-on-GCP-1-App-2-Agents-Add-On/main.tf
@@ -0,0 +1,47 @@
+module "App" {
+	source = "armdupre/module-ixload-app/google"
+	version = "9.36.0"
+	Eth0SubnetName = local.PublicSubnetName
+	Eth0VpcNetworkName = local.PublicVpcNetworkName
+	MachineType = local.AppMachineType
+	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = local.ZoneName
+}
+
+module "Agent1" {
+	source = "armdupre/module-ixload-agent/google"
+	version = "9.36.0"
+	Eth0SubnetName = local.PublicSubnetName
+	Eth0VpcNetworkName = local.PublicVpcNetworkName
+	Eth1SubnetName = local.PrivateSubnetName
+	Eth1VpcNetworkName = local.PrivateVpcNetworkName
+	InstanceId = local.Agent1InstanceId
+	MachineType = local.AgentMachineType
+	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = local.ZoneName
+}
+
+module "Agent2" {
+	source = "armdupre/module-ixload-agent/google"
+	version = "9.36.0"
+	Eth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
+	Eth0SubnetName = local.PublicSubnetName
+	Eth0VpcNetworkName = local.PublicVpcNetworkName
+	Eth1PrivateIpAddress = local.Agent2Eth1PrivateIpAddress
+	Eth1PrivateIpAliases = local.Agent2Eth1PrivateIpAliases
+	Eth1SubnetName = local.PrivateSubnetName
+	Eth1VpcNetworkName = local.PrivateVpcNetworkName
+	InstanceId = local.Agent2InstanceId
+	MachineType = local.AgentMachineType
+	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = local.ZoneName
+}
