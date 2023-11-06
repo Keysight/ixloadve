1bf7ed311b3a249f6b10106f40a5a17308acba18
armdupre <a-joseph.dupre@keysight.com>
2023-11-06 10:04:52 -0500

    Initial Checkin.

---
+++ gcp/Deployment/Terraform/IxLoad/IxLoad-on-GCP-1-App-1-Agent-2-VPCs-1-Public-Subnet-1-Private-Subnet/locals.tf
@@ -0,0 +1,11 @@
+locals {
+	AgentMachineType = var.AgentMachineType
+	Agent1InstanceId = "agent1"
+	AppMachineType = var.AppMachineType
+	AppTag = "ixload"
+	AppVersion = "9-36"
+	PublicFirewallRuleSourceIpRanges = var.PublicFirewallRuleSourceIpRanges
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+}
