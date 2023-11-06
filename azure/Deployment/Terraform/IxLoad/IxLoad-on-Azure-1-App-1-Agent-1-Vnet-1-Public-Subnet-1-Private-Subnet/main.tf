cd6625e067d64bb2558f201318a598c142b1445b
armdupre <a-joseph.dupre@keysight.com>
2023-11-06 09:15:55 -0500

    Initial Checkin.

---
+++ azure/Deployment/Terraform/IxLoad/IxLoad-on-Azure-1-App-1-Agent-1-Vnet-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -0,0 +1,41 @@
+module "App" {
+	source = "armdupre/module-ixload-app/azurerm"
+	version = "9.36.0"
+	AdminPassword = local.AppAdminPassword
+	AdminUserName = local.AppAdminUserName
+	Eth0SubnetId = module.Vnet.PublicSubnet.id
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	SshKeyName = azurerm_ssh_public_key.SshKey.name
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	VmSize = local.AppVmSize
+	depends_on = [
+		azurerm_ssh_public_key.SshKey,
+		module.Vnet
+	]
+}
+
+module "Agent1" {
+	source = "armdupre/module-ixload-agent/azurerm"
+	version = "9.36.0"
+	Eth0SubnetId = module.Vnet.PublicSubnet.id
+	Eth1SubnetId = module.Vnet.PrivateSubnet.id
+	InstanceId = local.Agent1InstanceId
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	SshKeyName = azurerm_ssh_public_key.SshKey.name
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	VmSize = local.AgentVmSize
+	depends_on = [
+		azurerm_ssh_public_key.SshKey,
+		module.App,
+		module.Vnet
+	]
+}
+
+resource "azurerm_resource_group" "ResourceGroup" {
+	name = local.ResourceGroupName
+	location = local.ResourceGroupLocation
+}
