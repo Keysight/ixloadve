6ab194fe00266e9176890fed89aa894943c4cf32
armdupre <a-joseph.dupre@keysight.com>
2023-11-06 09:02:04 -0500

    Initial Checkin.

---
+++ aws/Deployment/Terraform/IxLoad/IxLoad-on-AWS-1-App-2-Agents-1-VPC-1-Public-Subnet-2-Private-Subnets/vpc.tf
@@ -0,0 +1,22 @@
+module "Vpc" {
+	source = "armdupre/module-1-vpc-1-public-subnet-2-private-subnets/aws"
+	InboundIPv4CidrBlock = local.InboundIPv4CidrBlock
+	Private1SubnetAvailabilityZone = local.Private1SubnetAvailabilityZone
+	Private2SubnetAvailabilityZone = local.Private2SubnetAvailabilityZone
+	PublicSubnetAvailabilityZone = local.PublicSubnetAvailabilityZone
+	Region = local.Region
+	Tag = local.AppTag
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	Version = local.AppVersion
+}
+
+resource "aws_security_group_rule" "PublicIngress3389" {
+	type = "ingress"
+	security_group_id = module.Vpc.PublicSecurityGroup.id
+	protocol = "tcp"
+	from_port = 3389
+	to_port = 3389
+	cidr_blocks = [ local.InboundIPv4CidrBlock ]
+}
