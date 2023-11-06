882785eaf48082a6312cf8cd5f01a7ef94889a58
armdupre <a-joseph.dupre@keysight.com>
2023-11-06 10:08:50 -0500

    Initial Checkin.

---
+++ gcp/Deployment/Terraform/IxVM/IxVM-on-GCP-1-Agent-2-VPCs-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -0,0 +1,19 @@
+module "Agent1" {
+	source = "armdupre/module-ixload-agent/google"
+	version = "9.36.0"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	Eth1SubnetName = module.Vpc.PrivateSubnet.name
+	Eth1VpcNetworkName = module.Vpc.PrivateVpcNetwork.name
+	InstanceId = local.Agent1InstanceId
+	MachineType = local.AgentMachineType
+	RegionName = data.google_client_config.current.region
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = data.google_client_config.current.zone
+	depends_on = [
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
+	]
+}
