cd456584c535652457647e07406de33cbe9fdbb2
armdupre <a-joseph.dupre@keysight.com>
2024-01-12 09:27:55 -0500

    Updated.

--- aws/Deployment/Terraform/IxLoad/IxLoad-on-AWS-1-App-2-Agents-Add-On/variables.tf
+++ aws/Deployment/Terraform/IxLoad/IxLoad-on-AWS-1-App-2-Agents-Add-On/variables.tf
@@ -86,11 +86,13 @@ AppInstanceType must be one of the following types:
 
 variable "AwsAccessCredentialsAccessKey" {
 	description = "Access key component of credentials used for programmatic calls to AWS."
+	sensitive = true
 	type = string
 }
 
 variable "AwsAccessCredentialsSecretKey" {
 	description = "Secret access key component of credentials used for programmatic calls to AWS."
+	sensitive = true
 	type = string
 }
 
