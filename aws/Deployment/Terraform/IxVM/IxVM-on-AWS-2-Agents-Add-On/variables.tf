2e55cafe12c62f3d62141edcdbafdbd540565fcd
armdupre <a-joseph.dupre@keysight.com>
2024-01-16 08:34:22 -0500

    Updated.

--- aws/Deployment/Terraform/IxVM/IxVM-on-AWS-2-Agents-Add-On/variables.tf
+++ aws/Deployment/Terraform/IxVM/IxVM-on-AWS-2-Agents-Add-On/variables.tf
@@ -47,11 +47,13 @@ variable "ApiMaxRetries" {
 
 variable "AwsAccessCredentialsAccessKey" {
 	description = "Access key component of credentials used for programmatic calls to AWS."
+	sensitive = true
 	type = string
 }
 
 variable "AwsAccessCredentialsSecretKey" {
 	description = "Secret access key component of credentials used for programmatic calls to AWS."
+	sensitive = true
 	type = string
 }
 
