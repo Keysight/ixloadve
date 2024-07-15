1e26834fce1efb974f5a22b791dc54abf0219f7e
armdupre <a-joseph.dupre@keysight.com>
2024-07-15 09:25:59 -0400

    Updated.

--- microsoft-azure/Deployment/Terraform/IxLoad/IxLoad-on-AzureGovCloud-1-App-1-Agent-1-Vnet-1-Public-Subnet-1-Private-Subnet/locals.tf
+++ microsoft-azure/Deployment/Terraform/IxLoad/IxLoad-on-AzureGovCloud-1-App-1-Agent-1-Vnet-1-Public-Subnet-1-Private-Subnet/locals.tf
@@ -7,14 +7,14 @@ locals {
 	AppVmSize = var.AppVmSize
 	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.AppTag}"
 	PublicRdpSecurityRuleName = "${local.Preamble}-rdp-rule"
-	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes
+	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes == null ? [ "${data.http.ip.response_body}/32" ] : var.PublicSecurityRuleSourceIpPrefixes
 	ResourceGroupLocation = var.ResourceGroupLocation
-	ResourceGroupName = var.ResourceGroupName
+	ResourceGroupName = var.ResourceGroupName == null ? "${local.Preamble}-resource-group" : var.ResourceGroupName
 	SshKeyAlgorithm = "RSA"
 	SshKeyName = "${local.Preamble}-ssh-key"
 	SshKeyRsaBits = "4096"
 	SubscriptionId = var.SubscriptionId
-	UserEmailTag = var.UserEmailTag
-	UserLoginTag = var.UserLoginTag
-	UserProjectTag = var.UserProjectTag
+	UserEmailTag = var.UserEmailTag == null ? data.azurerm_client_config.current.client_id : var.UserEmailTag
+	UserLoginTag = var.UserLoginTag == null ? "terraform" : var.UserLoginTag
+	UserProjectTag = var.UserProjectTag == null ? random_id.RandomId.id : var.UserProjectTag
 }
