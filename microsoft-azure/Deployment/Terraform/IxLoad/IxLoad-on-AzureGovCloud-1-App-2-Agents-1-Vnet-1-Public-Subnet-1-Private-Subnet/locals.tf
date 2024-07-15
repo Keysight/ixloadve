654f1e16b7b20240e83ec71910710f7f5f513c00
armdupre <a-joseph.dupre@keysight.com>
2024-07-15 09:26:51 -0400

    Updated.

--- microsoft-azure/Deployment/Terraform/IxLoad/IxLoad-on-AzureGovCloud-1-App-2-Agents-1-Vnet-1-Public-Subnet-1-Private-Subnet/locals.tf
+++ microsoft-azure/Deployment/Terraform/IxLoad/IxLoad-on-AzureGovCloud-1-App-2-Agents-1-Vnet-1-Public-Subnet-1-Private-Subnet/locals.tf
@@ -2,7 +2,7 @@ locals {
 	AgentVmSize = var.AgentVmSize
 	Agent1InstanceId = "agent1"
 	Agent2Eth0IpAddress = "10.0.10.12"
-	Agent2Eth1IpAddresses = ["10.0.2.22", "10.0.2.23", "10.0.2.24", "10.0.2.25", "10.0.2.26", "10.0.2.27", "10.0.2.28", "10.0.2.29", "10.0.2.30", "10.0.2.31"]
+	Agent2Eth1IpAddresses = [ "10.0.2.22", "10.0.2.23", "10.0.2.24", "10.0.2.25", "10.0.2.26", "10.0.2.27", "10.0.2.28", "10.0.2.29", "10.0.2.30", "10.0.2.31" ]
 	Agent2InstanceId = "agent2"
 	AppAdminPassword = var.AppAdminPassword
 	AppAdminUserName = var.AppAdminUserName
@@ -10,14 +10,14 @@ locals {
 	AppVmSize = var.AppVmSize
 	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.AppTag}"
 	PublicRdpSecurityRuleName = "${local.Preamble}-rdp-rule"
-	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes
+	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes == null ? [ "${data.http.ip.response_body}/32" ] : var.PublicSecurityRuleSourceIpPrefixes
 	ResourceGroupLocation = var.ResourceGroupLocation
-	ResourceGroupName = var.ResourceGroupName
+	ResourceGroupName = var.ResourceGroupName == null ? "${local.Preamble}-resource-group" : var.ResourceGroupName
 	SshKeyAlgorithm = "RSA"
 	SshKeyName = "${local.Preamble}-ssh-key"
 	SshKeyRsaBits = "4096"
 	SubscriptionId = var.SubscriptionId
-	UserEmailTag = var.UserEmailTag
-	UserLoginTag = var.UserLoginTag
-	UserProjectTag = var.UserProjectTag
+	UserEmailTag = var.UserEmailTag == null ? data.azurerm_client_config.current.client_id : var.UserEmailTag
+	UserLoginTag = var.UserLoginTag == null ? "terraform" : var.UserLoginTag
+	UserProjectTag = var.UserProjectTag == null ? random_id.RandomId.id : var.UserProjectTag
 }
