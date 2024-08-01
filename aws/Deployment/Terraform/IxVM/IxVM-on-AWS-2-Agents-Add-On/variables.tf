0ef9060389c73562bcb9596e43a17bb9d55ce7b7
armdupre <a-joseph.dupre@keysight.com>
2024-08-01 00:24:10 -0400

    Updated.

--- aws/Deployment/Terraform/IxVM/IxVM-on-AWS-2-Agents-Add-On/variables.tf
+++ aws/Deployment/Terraform/IxVM/IxVM-on-AWS-2-Agents-Add-On/variables.tf
@@ -45,18 +45,6 @@ variable "ApiMaxRetries" {
 	type = number
 }
 
-variable "AwsAccessCredentialsAccessKey" {
-	description = "Access key component of credentials used for programmatic calls to AWS."
-	sensitive = true
-	type = string
-}
-
-variable "AwsAccessCredentialsSecretKey" {
-	description = "Secret access key component of credentials used for programmatic calls to AWS."
-	sensitive = true
-	type = string
-}
-
 variable "PrivateSecurityGroupName" {
 	description = "Security Group Name tag associated with the private subnet"
 	type = string
