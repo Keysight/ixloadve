c3db829771791611dae8fc5b0bf43c8d4d1dd9b5
armdupre <a-joseph.dupre@keysight.com>
2024-08-01 00:33:07 -0400

    Updated.

--- aws/Deployment/Terraform/IxLoad/IxLoad-on-AWS-1-App-2-Agents-1-VPC-1-Public-Subnet-1-Private-Subnet/vpc.tf
+++ aws/Deployment/Terraform/IxLoad/IxLoad-on-AWS-1-App-2-Agents-1-VPC-1-Public-Subnet-1-Private-Subnet/vpc.tf
@@ -1,6 +1,6 @@
 module "Vpc" {
 	source = "armdupre/module-1-vpc-1-public-subnet-1-private-subnet/aws"
-	version = "10.0.0"
+	version = "10.40.0"
 	InboundIPv4CidrBlocks = local.InboundIPv4CidrBlocks
 	PrivateSubnetAvailabilityZone = local.PrivateSubnetAvailabilityZone
 	PublicSubnetAvailabilityZone = local.PublicSubnetAvailabilityZone
